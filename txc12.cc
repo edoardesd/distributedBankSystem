33b0ad5b1dcb792a9e9c042e90d8b064e2342a89
edoardesd <edoardesd@gmail.com>
2017-05-29 18:00:57 +0200

    add msg structure

--- txc12.cc
+++ txc12.cc
@@ -1,9 +1,12 @@
 //
 // This file is part of an OMNeT++/OMNEST simulation example.
 //
-
+//how to compile:
 //opp_makemake -f -o output -I/mysql/include -L/mysql/lib/o -lmysqlclient
-
+//
+//how to run rapidly:
+//make && ./output
+//
 // Copyright (C) 2003 Ahmet Sekercioglu
 // Copyright (C) 2003-2015 Andras Varga
 //
@@ -17,81 +20,84 @@
 
 using namespace omnetpp;
 
-/**
- * Let's make it more interesting by using several (n) `tic' modules,
- * and connecting every module to every other. For now, let's keep it
- * simple what they do: module 0 generates a message, and the others
- * keep tossing it around in random directions until it arrives at
- * module 2.
- */
+#include "tictoc13_m.h"
+
+////////////////////// DATABASE METHODS //////////////////////
+
 class DB : public cSimpleModule
 {
   protected:
-    virtual void forwardMessage(cMessage *msg);
-    //virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
-};
+	virtual void forwardMessage(TicTocMsg13 *msg);
+	virtual void initialize() override;
+	virtual void handleMessage(cMessage *msg);
+	//virtual TicTocMsg13 *generateMessage();
 
+};
 
 
 Define_Module(DB);
 
 
-
+void DB::initialize()
+{
+	EV << "HI, I'm number " << getIndex() << "\n";
+	//printf("HI, I'm number %d \n", getIndex());
+	
+}
 
 void DB::handleMessage(cMessage *msg)
 {
-    if (getIndex() == 3) {
-        // Message arrived.
-        EV << "Message " << msg << " arrived.\n";
-        delete msg;
-    }
-    else {
-        // We need to forward the message.
-        forwardMessage(msg);
-    }
+	TicTocMsg13 *ttmsg = check_and_cast<TicTocMsg13 *>(msg);
+
+	if (getIndex() == 3) {
+		// Message arrived.
+		EV << "Message " << ttmsg << " arrived.\n";
+		delete ttmsg;
+	}
+	else {
+		// We need to forward the message.
+		forwardMessage(ttmsg);
+	}
 }
 
-void DB::forwardMessage(cMessage *msg)
+
+
+
+void DB::forwardMessage(TicTocMsg13 *msg)
 {
-    // In this example, we just pick a random gate to send it on.
-    // We draw a random number between 0 and the size of gate `gate[]'.
-    if (getIndex()==0){
-        
-        //send(msg, "gate$o", 2);
-        
-        int n = gateSize("gate");
-        for (int k = 0; k<n; k = k+1){
-            EV << "Forwarding message " << msg << " on gate[" << k << "]\n";
-            EV << "I'm number " << getIndex() << "\n";
-            // $o and $i suffix is used to identify the input/output part of a two way gate
-            cMessage *msg_forw = new cMessage("newmsg");
-            send(msg_forw, "gate$o", k);
-            bubble("send message");
-        }
-        delete msg;
-    }
-    else{
-    
-    int n = gateSize("gate");
-    int k = intuniform(0, n-1);
+	// In this example, we just pick a random gate to send it on.
+	// We draw a random number between 0 and the size of gate `gate[]'.
+	
+	// Increment hop count.
+    msg->setHopCount(msg->getHopCount()+1);
 
-    EV << "Forwarding message " << msg << " on gate[" << k << "]\n";
-    EV << "I'm number " << getIndex() << "\n";
+	int n = gateSize("gate");
+	int k = intuniform(0, n-1);
 
-    // $o and $i suffix is used to identify the input/output part of a two way gate
-    send(msg, "gate$o", k);
-    }
+	printf ("Gate size: %d\tDestination: %d \tIndex: %d\n", n, k, getIndex());
+
+	EV << "Forwarding message " << msg << " on gate[" << k << "]\n";
+	EV << "I'm number " << getIndex() << "\n";
+
+	//printf("Hop count: %d \n", msg->getHopCount());
+
+	// $o and $i suffix is used to identify the input/output part of a two way gate
+	send(msg, "gate$o", k);
+	
 }
 
+
+////////////////////// PEOPLE METHODS //////////////////////
 
 class Person : public cSimpleModule
 {
 
   protected:
-    virtual void forwardMessage(cMessage *msg);
-    virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
+	virtual void forwardMessage(TicTocMsg13 *msg);
+	virtual void initialize() override;
+	virtual void handleMessage(cMessage *msg);
+	virtual TicTocMsg13 *generateMessage(int src, int dest);
+
 };
 
 Define_Module(Person);
@@ -100,60 +106,72 @@ Define_Module(Person);
 
 void Person::initialize()
 {
-    if (par("sendMsgOnInit").boolValue() == true) {
-        // Boot the process scheduling the initial message as a self-message.
-        EV << "Sending initial mussage from number " << getIndex() <<"\n";
-        char msgname[20];
-        sprintf(msgname, "msg-%d", getIndex()); //getIndex is the number that identify the node (itself)
-        cMessage *msg_self = new cMessage(msgname);
-        scheduleAt(0.0, msg_self);
-        //send(msg, "gate$o", 2);
-    }
+	printf ("my gatesize %d", gateSize("gateP"));
+	EV << "HI, I'm number " << getIndex() << "\n";
+	printf("HI, I'm number %d \n", getIndex());
+	if (par("sendMsgOnInit").boolValue() == true) {
+		// Boot the process scheduling the initial message as a self-message.
+		EV << "Sending initial self message from " << getName() <<"\n";
+		//char msgname[20];
+		//sprintf(msgname, "msg-%s", getName()); //getIndex is the number that identify the node (itself)
+        int n = gateSize("gateP");
+		for (int k = 0; k<n; k = k+1){
+			// $o and $i suffix is used to identify the input/output part of a two way gate
+			TicTocMsg13 *msg_forw = generateMessage(getIndex(), k);
+			send(msg_forw, "gateP$o", k);
+			printf("Forwarding message on gate[%d]\n", k);
+
+			bubble("send message");
+
+			//send(msg, "gate$o", 2);
+		}
+	}
 }
-void Person::forwardMessage(cMessage *msg)
+
+void Person::forwardMessage(TicTocMsg13 *msg)
 {
-    // In this example, we just pick a random gate to send it on.
-    // We draw a random number between 0 and the size of gate `gate[]'.
-    if (getIndex()==0){
-        
-        //send(msg, "gate$o", 2);
-        
-        int n = gateSize("gate");
-        for (int k = 0; k<n; k = k+1){
-            EV << "Forwarding mussasasas " << msg << " on gate[" << k << "]\n";
-            EV << "I'm number " << getIndex() << "\n";
-            // $o and $i suffix is used to identify the input/output part of a two way gate
-            cMessage *msg_forw = new cMessage("newmsg");
-            send(msg_forw, "gate$o", k);
-            bubble("send message");
-        }
-        delete msg;
-    }
-    else{
-    
-    int n = gateSize("gate");
-    int k = intuniform(0, n-1);
+	// In this example, we just pick a random gate to send it on.
+	// We draw a random number between 0 and the size of gate `gate[]'.
+	//EV << "I'm number " << getIndex() << "\n";
 
-    EV << "Forwarding message " << msg << " on gate[" << k << "]\n";
-    EV << "I'm number " << getIndex() << "\n";
+	int n = gateSize("gateP");
+	for (int k = 0; k<n; k = k+1){
+		// $o and $i suffix is used to identify the input/output part of a two way gate
+		TicTocMsg13 *msg_forw = generateMessage(getIndex(), k);
+		send(msg_forw, "gateP$o", k);
+		printf("Forwarding message on gate[%d]\n", k);
 
-    // $o and $i suffix is used to identify the input/output part of a two way gate
-    send(msg, "gate$o", k);
-    }
+		bubble("send message");
+	}
+	//delete msg;
+	
 
 }
 
 void Person::handleMessage(cMessage *msg)
 {
-    if (getIndex() == 3) {
-        // Message arrived.
-        EV << "Message " << msg << " arrived.\n";
-        delete msg;
-    }
-    else {
-        // We need to forward the message.
-        forwardMessage(msg);
-    }
+    TicTocMsg13 *ttmsg = check_and_cast<TicTocMsg13 *>(msg);
+
+    printf("Msg from %d come back with destination %d\n", ttmsg->getSource(), ttmsg->getDestination());
+	//forwardMessage(ttmsg);
+
+}
+
+TicTocMsg13 *Person::generateMessage(int src, int dest)
+{
+    // Produce source and destination addresses.
+    //int src = getIndex();  // our module index
+    //int n = 6;  // module vector size
+    //int dest = intuniform(0, n-1);
+    //if (dest >= src)
+    //    dest++;
+    //char msgname[20];
+    //sprintf(msgname, "tic-%d-to-%d", src, dest);
+    // Create message object and set source and destination field.
+    TicTocMsg13 *msg = new TicTocMsg13("msg_gen");
+    msg->setSource(src);
+    msg->setDestination(dest);
+    return msg;
 }
 
 
